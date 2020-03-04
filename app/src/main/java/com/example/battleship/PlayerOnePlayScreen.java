10a45cdb0df784578783abe00b491dc9f9e8a245
Brad <bpendleton@student.neumont.edu>
2020-03-04 09:25:14 -0700

    Button Arrays in Play Screens

--- app/src/main/java/com/example/battleship/PlayerOnePlayScreen.java
+++ app/src/main/java/com/example/battleship/PlayerOnePlayScreen.java
@@ -3,15 +3,36 @@ package com.example.battleship;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class PlayerOnePlayScreen extends AppCompatActivity {
+public class PlayerOnePlayScreen extends AppCompatActivity implements View.OnClickListener{
+
+    private Button[][] buttons = new Button[10][10];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_one_play_screen);
+
+        for(int row = 0; row < 10; row++) {
+            for(int col = 0; col < 10; col++) {
+                String buttonID = "button_" + row + col;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[row][col] = findViewById(resID);
+                buttons[row][col].setOnClickListener(this);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (((Button) v).getText().toString().equals("")) {
+            ((Button) v).setBackgroundColor(Color.BLUE);
+        }
     }
 
     //View own board with contentview
