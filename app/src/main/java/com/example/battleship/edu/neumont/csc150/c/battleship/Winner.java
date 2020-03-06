d650b66940140b86a9a143fa105fee5af0cf3de0
Brad <bpendleton@student.neumont.edu>
2020-03-06 08:51:36 -0700

    Winner Screen

---
+++ app/src/main/java/com/example/battleship/edu/neumont/csc150/c/battleship/Winner.java
@@ -0,0 +1,16 @@
+package com.example.battleship.edu.neumont.csc150.c.battleship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.battleship.R;
+
+public class Winner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winner);
+    }
+}
